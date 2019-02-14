0245f3d40d690c0f8092f8b9e483679e45c79f1f
minhoCha <minhocha1004@gmail.com>
2019-02-13 20:49:39 -0800

    commit

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Hello World');
+const express = require('express');
+const axios = require('axios');
+
+const app = express()
+const port = 3000
+
+// end point
+app.get('/hello', (req, res) => {
+    console.log(req.query);
+    res.send('world' + req.query.name);
+});
+
+app.get('/network', (req, res) => {
+    axios.get('http://localhost:3001/world')
+    .then((res) => {
+        res.send(networkResponse.data);
+    }) // if it doesn't work
+    .catch(() => {
+        res.send(':( did not work.');
+    });
+});
+
+
+app.get('/add', (req, res) => res.send('0'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
